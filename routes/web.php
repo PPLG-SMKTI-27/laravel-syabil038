7600201d5e2687f913624496fcf8a24ff9ebf096
syabil038 <north2129@gmail.com>
2026-01-19 15:52:21 +0800

    tugas 2

---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+use App\Http\Controllers\ProjectController;
+
+Route::get('/project', [ProjectController::class, 'index']);
